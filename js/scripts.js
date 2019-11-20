a2e7a1acadcab6b05bf7bc2d609fd813bbb8da56
Amy Milov <iamamymilov@gmail.com>
2019-11-20 15:14:36 -0800

    boba page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 console.log('Hello from js/scripts.js!');
+
+let submit = document.getElementById('Submit');
+let firstName = document.getElementById('inputFirstName');
+
+
+submit.addEventListener('click', customName);
+
+function customName() {  if(firstName.value !== '') {
+    let name = firstName.value;
+    let message = 'Thank you  ' + name + '!!! Enjoy Your Free Drink On Us!';
+    let customMessage = document.querySelector('.customMessage');
+    customMessage.textContent = message;
+
+  }
+}
